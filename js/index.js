0e90b67574d90d54a99ef363f4d25ad1cd146d60
ktaber <Taber.kathryn@gmail.com>
2017-11-02 20:38:03 -0400

    Starter Matchup responsive

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+// $('.hamburgerMenu').click(function() {
+//     $('nav').slideDown('fast');
+//     event.preventDefault();
+// });
+
+// $('.hamburgerMenu').click(function() {
+//     $('nav').slideUp('slow');
+//     event.preventDefault();
+// });
+
+$('.hamburgerMenu').click(function(){
+    $('nav').slideToggle(200);
+});
